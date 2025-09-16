d56922e806f6085511f6ad3aa1581da341791b67
Berners <berners.lk@gmail.com>
2025-09-16 16:49:22 +0800

    sync test fold

--- src/netknife/__init__.py
+++ src/netknife/__init__.py
@@ -1,3 +1,5 @@
-﻿__all__ = []
-__version__ = "1.1.0"  
-
+﻿try:
+    from importlib.metadata import version, PackageNotFoundError
+    __version__ = version("netknife")
+except PackageNotFoundError:  # during editable/dev
+    __version__ = "1.1.0"
